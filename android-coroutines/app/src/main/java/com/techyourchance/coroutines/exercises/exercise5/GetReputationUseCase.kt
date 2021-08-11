45cc133352523ed86c467d3b9d84288288aa8914
Raul Guzman <rguzmanc.161@gmail.com>
2021-08-11 11:57:34 -0500

    Feature: Add coroutine course

---
+++ android-coroutines/app/src/main/java/com/techyourchance/coroutines/exercises/exercise5/GetReputationUseCase.kt
@@ -0,0 +1,8 @@
+package com.techyourchance.coroutines.exercises.exercise5
+
+import com.techyourchance.coroutines.exercises.exercise1.GetReputationEndpoint
+
+class GetReputationUseCase(getReputationEndpoint: GetReputationEndpoint) {
+
+
+}
